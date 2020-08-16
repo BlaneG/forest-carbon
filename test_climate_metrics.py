ccf10bc8608bb0094c9ef30d0f1147af4d2a635b
Blane Grann <blanegrann@gmail.com>
2020-08-16 15:54:51 -0700

    ENH: added test file

---
+++ test_climate_metrics.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from climate_metrics import AGWP_CO2, AGWP_CH4_no_CO2
+
+
+def test_AGWP_CO2():
+    """
+    References
+    ----------
+    IPCC, 2013. AR5, WG1, Chapter 8.  Appendix 8.A.
+    https://www.ipcc.ch/report/ar5/wg1/
+    """
+    assert np.isclose(AGWP_CO2(100)/9.17e-14, 1, atol=1e-02)
+    assert np.isclose(AGWP_CO2(20)/2.49e-14, 1, atol=1e-02)
+
+
+def test_AGWP_CH4_no_CO2():
+    """
+    References
+    ----------
+    IPCC, 2013. AR5, WG1, Chapter 8.  Appendix 8.A.
+    https://www.ipcc.ch/report/ar5/wg1/
+    """
+    assert np.isclose(AGWP_CH4_no_CO2(20)/2.09e-12, 1, atol=1e-03)
+    assert np.isclose(AGWP_CH4_no_CO2(100)/2.61e-12, 1, atol=1e-03)
